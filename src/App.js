3655ad7ff97bc1fb452a8c9b2a8474e86d2b2c9e
Nasiru <nasir.moh.ibrahim@gmail.com>
2021-01-20 18:09:13 +0100

    added state management

--- src/App.js
+++ src/App.js
@@ -7,12 +7,14 @@ const test = require("leaflet")
 const MAP_BOX_KEY = 'pk.eyJ1IjoibmFzaXJpYnJhaGltIiwiYSI6ImNrazVtcXI3ajA4bGYycG53YzF5YmU2czkifQ.cF5THuhE-nBpxe19wNZAcg'
 function App() {
 
+  const [inputValue, setInputValue] = React.useState('')
+
+
   React.useEffect(() => {
     const map = LeafletMap.map('map', {
       center: [51.505, -0.09],
       zoom: 13
     })
-
     LeafletMap.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
       attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
       maxZoom: 18,
@@ -23,8 +25,18 @@ function App() {
     }).addTo(map);
   }, [])
 
+
   return (
     <div className="App">
+      <input
+        onChange={(e) => {
+          e.preventDefault()
+          setInputValue(e.target.value)
+          console.log(e.target.value)
+        }}
+
+        value={inputValue}
+      />
       <div className="img-bg"></div>
       <div id="map"></div>
     </div>
